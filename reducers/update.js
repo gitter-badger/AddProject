6a507656cf0580eba9df9d3e000dd08146b1aee1
Vaiginta Perkauskaite <vaiginta@gmail.com>
2016-01-15 11:52:23 +0000

    updated reducer

--- reducers/update.js
+++ reducers/update.js
@@ -1,10 +1,17 @@
 import { ADD_PROJECT } from '../actions/update'
 import Immutable from 'immutable'
 
-export default (state = Immutable.List(['']), action) => {
+export default function updateProject (state = Immutable.List(['']), action)  {
   switch(action.type) {
     case ADD_PROJECT:
-      return state.push(action.id + ' ' + action.name + ' ' + action.date + ' ' + action.scenario)
+      return [
+      ...state,
+      action.id,
+      action.name,
+      action.date,
+      action.scenario
+
+      ]
     default:
       return state
   }
